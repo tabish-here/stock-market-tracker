4c3920153b52c9764448619c68dce67507022a45
tabish-here <ansaritabish192@gmail.com>
2024-02-27 22:17:15 +0530

    add stock API credentials and logic for making request and handling rsponse

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+from twilio.rest import Client
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "Your_stock_endpoint"
+NEWS_ENDPOINT = "Your_news_endpoint"
+
+STOCK_API_KEY = "Your_stock_api_key"
+NEWS_API_KEY = "Your_news_api_key"
+account_sid = "Your_account_sid_number"
+auth_token = "Your_auth_token"
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY
+}
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
+difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+print(difference)
+diff_percent = difference / float(yesterday_closing_price) * 100
+print(diff_percent)
+
+if diff_percent > 0:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
+    print(three_articles)
+
+    
+
